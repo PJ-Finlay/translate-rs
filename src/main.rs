0196207ad7749637c9937d6d6f73a29784fa114e
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-20 23:54:24 +0000

    Get environment variable

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use std::process::Command;
 
 fn main() {
     println!("Hello World!!");
+
+    // Run command
     let mut command = Command::new("ls");
     command.status().expect("process failed to execute");
+
+    // Get environment variable
+    // https://programming-idioms.org/idiom/205/get-an-environment-variable/3700/rust
+    let _foo = match std::env::var("FOO") {
+        Ok(val) => val,
+        Err(_e) => "none".to_string(),
+    };
 }
