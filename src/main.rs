4196b8867caa994110e30e7a681446de4fedb4a1
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-21 00:17:08 +0000

    Added translating (Not compiling)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use std::process::Command;
 
 fn main() {
-    println!("Hello World!!");
-
-    // Run command
-    let mut command = Command::new("ls");
-    command.status().expect("process failed to execute");
-
     // Get environment variable
     // https://programming-idioms.org/idiom/205/get-an-environment-variable/3700/rust
     let _foo = match std::env::var("FOO") {
         Ok(val) => val,
         Err(_e) => "none".to_string(),
     };
+
+    // Run command
+    let translate_command = "~/CTranslate2/build/cli/translate --model ~/argos-translate/en_zh/model";
+
+    // let translate_command = "ls";
+
+    let mut command = Command::new(translate_command);
+    command.status().expect("process failed to execute");
+
+    // Print line
+    println!("Hello World");
 }
