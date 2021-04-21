a4c735ba2e5fd101cd3fed9cf78ef38fe0c5381d
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-21 01:31:18 +0000

    Translating with CTranslate2 model working

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
 use std::process::Command;
 
 fn main() {
-    // Get environment variable
-    // https://programming-idioms.org/idiom/205/get-an-environment-variable/3700/rust
-    let _foo = match std::env::var("FOO") {
-        Ok(val) => val,
-        Err(_e) => "none".to_string(),
-    };
-
     // Run command
-    let translate_command = "~/CTranslate2/build/cli/translate --model ~/argos-translate/en_zh/model";
-
-    // let translate_command = "ls";
-
-    let mut command = Command::new(translate_command);
+    // ~/CTranslate2/build/cli/translate --model ~/argos-translate/en_zh/model
+    let ctranslate = "/home/argosopentech/CTranslate2/build/cli/translate";
+    let mut command = Command::new(ctranslate);
+    command.arg("--model");
+    command.arg("/home/argosopentech/argos-translate/en_zh/model");
     command.status().expect("process failed to execute");
-
-    // Print line
-    println!("Hello World");
 }
